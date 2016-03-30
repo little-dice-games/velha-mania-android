d5bb6d07b56773d6e7688551422540f191b8020f
Leonardo Tegon <ltegon93@gmail.com>
2016-03-29 21:10:21 -0300

    Add crosswalk view

---
+++ app/src/main/java/br/com/velhamania/XWalkActivity.java
@@ -0,0 +1,54 @@
+package br.com.velhamania;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkView;
+
+public class XWalkActivity extends Activity {
+    private XWalkView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xwalk);
+
+        if (BuildConfig.DEBUG) {
+            XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+        }
+
+        mWebView = (XWalkView) findViewById(R.id.webView);
+        mWebView.load("http://velhamania.com.br", null);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mWebView != null) {
+            mWebView.pauseTimers();
+            mWebView.onHide();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mWebView != null) {
+            mWebView.resumeTimers();
+            mWebView.onShow();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mWebView != null) {
+            mWebView.load("about:blank", null);
+            mWebView.onDestroy();
+        }
+    }
+}
